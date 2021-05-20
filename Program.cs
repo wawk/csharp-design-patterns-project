a53a2fc1957fc926c63b1e752a335fe035e03be9
wawk <white_knight@prodigy.net>
2021-05-20 08:11:56 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace csharp_design_patterns_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
